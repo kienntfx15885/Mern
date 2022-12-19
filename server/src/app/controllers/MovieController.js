10adfeff0ef0089bde7514d991ecf3343bc929ea
kienntfx15885 <kienntfx15885@funix.edu.vn>
2022-12-19 18:07:56 +0700

    Update file

--- server/src/app/controllers/MovieController.js
+++ server/src/app/controllers/MovieController.js
@@ -2,12 +2,14 @@
 const Movie = require('../../resources/models/Movie')
 
 const MovieController = {
-    show: function (req, res, next) {
-        Movie.find({})
-            .then(movies => {
-                res.send(movies)
-            })
-            .catch(next)
+    show: async function (req, res, next) {
+        try {
+            const movies = await Movie.find({user: req.userId}).populate('user', ['username'])
+            res.json({success: true, movies})
+        } catch (err) {
+            console.log(err)
+            res.status(500).json({success: false, message: 'Internal server error'})
+        }
     },
 
     store: async function (req, res) {
@@ -31,6 +33,40 @@ const MovieController = {
             await newMovie.save()
 
             res.json({success: true, message: 'Happy watch', movie: newMovie})
+        } catch (err) {
+            console.log(err)
+            res.status(500).json({success: false, message: 'Internal server error'})
+        }
+    },
+
+    update: async function (req, res) {
+        const {name, description, videoId, image, studio} = req.body
+
+        // Simple validation
+        if (!name) {
+            return res.status(400).json({success: false, message: 'Name is required'})
+        }
+
+        try {
+            let updatedMovie = {
+                name,
+                description: description || '',
+                videoId,
+                image,
+                studio: studio || 'MARVEL',
+
+            }
+
+            const movieUpdateCondition = {_id: req.params._id, user: req.userId}
+
+            updatedMovie = await Movie.findOneAndUpdate(movieUpdateCondition, updatedMovie, {updated: true})
+
+            // User not authorized to update movie or movie not found
+            if (!updatedMovie) {
+                return res.status(401).json({success: false, message: 'Movie not found or user not authorize'})
+            }
+
+            res.json({success: true, message: 'Excellent progress', movie: updatedMovie})
         } catch (err) {
             console.log(err)
             res.status(500).json({success: false, message: 'Internal server error'})
--- server/src/routes/movie.js
+++ server/src/routes/movie.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const movieController = require('../app/controllers/MovieController')
-const verifyToken = require('../middleware/auth')
+const verifyToken = require('../app/middleware/auth')
 
 router.post('/store', verifyToken, movieController.store)
 
-router.get('/', movieController.show)
+router.put('/:_id', verifyToken, movieController.update)
+
+router.get('/', verifyToken, movieController.show)
 
 module.exports = router
